ba352ad9ea5cece79e0e7ac97b24878d1110d956
Alex Wilson <wilson@loseit.com>
2016-11-02 10:13:21 -0400

    Use fragment tags to search for fragments

--- Fragments/app/src/main/java/com/example/alexwilson/smellslikebakin/MainActivity.java
+++ Fragments/app/src/main/java/com/example/alexwilson/smellslikebakin/MainActivity.java
@@ -8,17 +8,20 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.OnRecipeSelectedInterface {
 
+    public static final String LIST_FRAGMENT = "list_fragment";
+    public static final String VIEWPAGER_FRAGMENT = "viewpager_fragment";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListFragment savedFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.placeholder);
+        ListFragment savedFragment = (ListFragment) getSupportFragmentManager().findFragmentByTag(LIST_FRAGMENT);
         if (savedFragment == null) {
             ListFragment fragment = new ListFragment();
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.placeholder, fragment);
+            fragmentTransaction.add(R.id.placeholder, fragment, LIST_FRAGMENT);
             fragmentTransaction.commit();
         }
     }
@@ -33,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements ListFragment.OnRe
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.placeholder, fragment);
+        fragmentTransaction.replace(R.id.placeholder, fragment, VIEWPAGER_FRAGMENT);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
