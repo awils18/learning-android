a4e3cc02b4222dc31fdb849454ad5e2461efd049
Alex Wilson <wilson@loseit.com>
2016-11-02 10:33:35 -0400

    Add direction and ingredients tabs

---
+++ Fragments/app/src/main/java/com/example/alexwilson/smellslikebakin/IngredientsFragment.java
@@ -0,0 +1,21 @@
+package com.example.alexwilson.smellslikebakin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by alexwilson on 11/2/16.
+ */
+
+public class IngredientsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_ingredients, container, false);
+        return view;
+    }
+}
--- Fragments/app/src/main/java/com/example/alexwilson/smellslikebakin/ViewPagerFragment.java
+++ Fragments/app/src/main/java/com/example/alexwilson/smellslikebakin/ViewPagerFragment.java
@@ -2,7 +2,10 @@ package com.example.alexwilson.smellslikebakin;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +28,31 @@ public class ViewPagerFragment extends Fragment {
         int index = getArguments().getInt(KEY_RECIPE_INDEX);
         getActivity().setTitle(Recipes.names[index]);
         View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
+        final IngredientsFragment ingredientsFragment = new IngredientsFragment();
+        final DirectionsFragment directionsFragment = new DirectionsFragment();
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return position == 0 ? ingredientsFragment : directionsFragment;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return position == 0 ? "Ingredients" : "Directions";
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
         return view;
     }
 
