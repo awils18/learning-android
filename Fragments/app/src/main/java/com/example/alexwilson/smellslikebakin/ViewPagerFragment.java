5a728b14ba9fa4ac68b64389a552ed1b379ab3e6
Alex Wilson <wilson@loseit.com>
2016-11-03 10:37:20 -0400

    Add directions checkboxes

--- Fragments/app/src/main/java/com/example/alexwilson/smellslikebakin/ViewPagerFragment.java
+++ Fragments/app/src/main/java/com/example/alexwilson/smellslikebakin/ViewPagerFragment.java
@@ -33,7 +33,11 @@ public class ViewPagerFragment extends Fragment {
         Bundle bundle = new Bundle();
         bundle.putInt(KEY_RECIPE_INDEX, index);
         ingredientsFragment.setArguments(bundle);
+
         final DirectionsFragment directionsFragment = new DirectionsFragment();
+        bundle = new Bundle();
+        bundle.putInt(KEY_RECIPE_INDEX, index);
+        directionsFragment.setArguments(bundle);
 
         ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager);
         viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
